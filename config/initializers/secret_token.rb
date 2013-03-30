8345a5a67c7dc4c2197a339db919889117dbc7ad
Igor Ribeiro Lima <lima.igorribeiro@gmail.com>
2013-03-30 17:01:04 -0300

    Init Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GiftCardsChallenge::Application.config.secret_token = 'ba5e5fe0d39ab2a864191d5518613c7b877a415c93f85b067db1ef923802ecbcb38aa0d31b7a7bbbe5b53c0ea6fcc43686828dc227eeaef250be38e9852d394e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GiftCardsChallenge::Application.config.session_store :cookie_store, key: '_gift-cards-challenge_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GiftCardsChallenge::Application.config.session_store :active_record_store

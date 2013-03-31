e378e48a535664b56ba82deed7a9368adf8e15fe
Igor Ribeiro Lima <lima.igorribeiro@gmail.com>
2013-03-31 09:17:32 -0300

    Add test for reporting git cards

--- test/functional/reports_controller_test.rb
+++ test/functional/reports_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class ReportsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should get gift_cards" do
+    get :gift_cards
+    assert_response :success
+  end
+
 end

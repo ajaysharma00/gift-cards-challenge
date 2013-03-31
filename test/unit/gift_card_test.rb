bc7e6a29c9653f8dcf5f50b0695406dc18f5c5c0
Igor Ribeiro Lima <lima.igorribeiro@gmail.com>
2013-03-31 07:49:21 -0300

    Refactor

--- test/unit/gift_card_test.rb
+++ test/unit/gift_card_test.rb
@@ -26,7 +26,7 @@ class GiftCardTest < ActiveSupport::TestCase
   test "How many times some person has given a gift" do
     report = GiftCard.report
 
-    assert_equal 2, report["Igor"].gifts_given, "Igor given gifts only for two people"
+    assert_equal 2, report["Igor"][:gifts_given], "Igor given gifts only for two people"
   end
 
 end

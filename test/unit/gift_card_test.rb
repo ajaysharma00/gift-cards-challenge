0bc5901fda30836edf625511556ecede8a012838
Igor Ribeiro Lima <lima.igorribeiro@gmail.com>
2013-03-31 07:02:51 -0300

    Test GiftCard.senders

--- test/unit/gift_card_test.rb
+++ test/unit/gift_card_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class GiftCardTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  # called before every single test
+  setup do
+    GiftCard.delete_all
+    GiftCard.new( { :sender => "Igor", :receiver => "Paulo", :descripion => "Gift for his birthday", :value => 3.01 } ).save
+    GiftCard.new( { :sender => "Paulo", :receiver => "Jose", :descripion => "Gift for his birthday", :value => 2.99 } ).save
+    GiftCard.new( { :sender => "Igor", :receiver => "Maria", :descripion => "Easter", :value => 4.01 } ).save
+  end
+
+  # called after every single test
+  def teardown
+
+  end
+
+  test "size of distinct values for a single sender" do
+    senders = GiftCard.senders
+
+    assert_equal 2, senders.size, "The size of sender must be '2'"
+  end
+
 end

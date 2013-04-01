6768d6da508b00066564f0d40a2b953a140ce6f0
Igor Lima <igor.lima@evolut.io>
2013-04-01 01:02:18 -0300

    Refactor

--- features/step_definitions/text_input.rb
+++ features/step_definitions/text_input.rb
@@ -6,21 +6,21 @@ end
 
 module GC
 
-  def self.type_on_label driver, content, label
-    TextInput.new.type driver, content, label
+  def self.type_on_label driver, content, text_input_label
+    TextInput.new.type driver, content, text_input_label
   end
 
   class TextInput
 
-    def type driver, content, label_name
-      text_input = find_text_input driver, label_name
+    def type driver, content, text_input_label
+      text_input = find_text_input driver, text_input_label
       text_input.send_keys content
     end
 
     private
 
-    def find_text_input driver, label_name
-      label = find_label driver, label_name
+    def find_text_input driver, text_input_label
+      label = find_label driver, text_input_label
       driver.execute_script("el = arguments[0]; while (el.tagName != 'INPUT') { el = el.nextSibling }; return el;", label)
     end
 

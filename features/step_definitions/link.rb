4d8e23689361132da082e820e47e678b4635ab1b
Igor Lima <igor.lima@evolut.io>
2013-03-31 23:40:30 -0300

    Implement link step definition

--- features/step_definitions/link.rb
+++ features/step_definitions/link.rb
@@ -1,19 +1,26 @@
 # encoding: utf-8
 
-When /^ click on "([^\"]*)"$/ do |link_name|
-
+When /^click on "([^\"]*)"$/ do |link_name|
+  GC.click_on_link @driver, link_name
 end
 
 module GC
 
-  def self.link link_name
-    Link.new.find link_name
+  def self.click_on_link driver, link_name
+    Link.new.click driver, link_name
   end
 
   class Link
 
-    def find link_name
+    def click driver, link_name
+      link = find( driver, link_name )
+      link.click
+    end
 
+    private
+
+    def find driver, link_name
+      driver.find_element(:link_text, link_name)
     end
 
   end
